24839713c1bdc76f0948eb67171c48ef72237264
marzhanaccessteacher2021-png <marzhanaccessteacher2021@gmail.com>
2025-12-09 23:24:20 +0500

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header style={{
+      padding: '16px',
+      backgroundColor: '#282c34',
+      color: 'white'
+    }}>
+      <h1>TaskNest</h1>
+      <p>Ваш центр управления задачами</p>
+    </header>
+  );
+}
+
+export default Header;
